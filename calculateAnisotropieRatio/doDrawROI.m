fcb54a23a170940442247c2da29a3d4633e17410
Spira,Felix Gerhard <felix.spira@imba.oeaw.ac.at>
2014-02-14 17:21:48 +0100

    no message

---
+++ calculateAnisotropieRatio/doDrawROI.m
@@ -0,0 +1,18 @@
+function imgMergeSaveVertical = doDrawROI(imgMerge,BWVer,analysisFrame)
+
+BW2 = edge(BWVer,'canny',0.1,0.1);
+BW3 = bwboundaries(BW2);
+BW3Work = BW3{1};
+
+BWTest = BW2;
+BWTest(:,:) =0;
+
+
+imgMergeSaveVertical = imgMerge(:,:,:,analysisFrame);
+
+
+for lauf = 1:length(BW3Work)
+    
+    imgMergeSaveVertical(BW3Work(lauf,1),BW3Work(lauf,2),:,:) = 255;
+    
+end
